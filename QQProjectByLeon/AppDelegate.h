5364da4d266a1708959df4141a02af425e76ed65
吴亮 <mac@leondeimac.lan>
2018-07-21 16:06:57 +0800

    Initial Commit

---
+++ QQProjectByLeon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QQProjectByLeon
+//
+//  Created by  Mac on 2018/7/21.
+//  Copyright © 2018年 Leon.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ QQProjectByLeon/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  QQProjectByLeon
+//
+//  Created by  Mac on 2018/7/21.
+//  Copyright © 2018年 Leon.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
